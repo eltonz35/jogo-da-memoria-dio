8c3a082a870d0727872dd2bb65f3bb131a227767
Elthon <eltonsilva.mecatronic@gmail.com>
2022-07-18 16:21:26 -0300

    adicionando forEach

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cards = document.querySelectorAll(".card");
 function flipcard() {
     this.classList.toggle("flip");
 }
+
+cards.forEach((card) => {  //Percorre toda a lista de cartas
+    card.addEventListener("click", flipcard);
+})
