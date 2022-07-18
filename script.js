c1b98d9b495275d4239dabd5108e83f95ece2262
Elthon <eltonsilva.mecatronic@gmail.com>
2022-07-18 18:41:15 -0300

    adicionando nova function checkForMatch

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ function flipCard() {
     }
 
     secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() { //função que desabilita o click, casa as cartas sejam iguais
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
 }
 
 cards.forEach((card) => {  //Percorre toda a lista de cartas
