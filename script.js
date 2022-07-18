84ca82f9f2f05ebcc316536187cd0f66049c9513
Elthon <eltonsilva.mecatronic@gmail.com>
2022-07-18 18:25:07 -0300

    adicionando novas variaveis e modificando o flipCard

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const cards = document.querySelectorAll(".card");
+let hasFlippdCard = false;
+let firstCard, secondCard;
 
-function flipcard() {
-    this.classList.toggle("flip");
+function flipCard() {
+    this.classList.add("flip");
+    if(!hasFlippdCard) {
+        hasFlippdCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
 }
 
 cards.forEach((card) => {  //Percorre toda a lista de cartas
-    card.addEventListener("click", flipcard);
-})
+    card.addEventListener("click", flipCard);
+});
